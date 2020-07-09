7e3fdbb54cc1ecca8c5207a41c14c53c16450490
15711657500 <15711657500@163.com>
2020-07-09 20:50:55 +0800

    a possible answer

--- code/decode/Decode.sv
+++ code/decode/Decode.sv
@@ -65,8 +65,8 @@ module decode (
 						  (dataD.instr.op == SRAV) || (dataD.instr.op == SRA) ||
 						  (dataD.instr.op == SRLV) || (dataD.instr.op == SRL) ||
 						  (dataD.instr.op == LUI);
-	maindec mainde(dataF.instr_, dataD.instr.op, dataD.exception_ri);
-	aludec alude(dataD.instr.op, dataD.instr.ctl.alufunc);
+	maindec mainde(dataF.instr_, dataD.instr.op, dataD.exception_ri, dataD.instr.ctl);
+	// aludec alude(dataD.instr.op, dataD.instr.ctl.alufunc);
 	assign is_reserved = dataD.instr.op == RESERVED;
 	assign pcbranch = dataF.pcplus4 + {dataD.instr.extended_imm[29:0], 2'b00};
 	assign pcjump = {dataF.pcplus4[31:28], dataF.instr_[25:0], 2'b00};
