29bfde105abcfcefe3b6c7e5a1a56f6873a2a1c3
15711657500 <15711657500@163.com>
2020-07-11 20:30:46 +0800

    modify top module

--- code/mycpu_top.sv
+++ code/mycpu_top.sv
@@ -1,22 +1,19 @@
 `include "mips.svh"
 
 // sram
-module mycpu_top(
+module mycpu(
     input logic clk,
     input logic resetn,  //low active
     input logic[5:0] ext_int,  //interrupt,high active
 
-    output logic inst_sram_en,
-    output rwen_t inst_sram_wen,
-    output word_t inst_sram_addr,
-    output word_t inst_sram_wdata,
-    input word_t inst_sram_rdata,
-    
-    output logic data_sram_en,
-    output rwen_t data_sram_wen,
-    output word_t data_sram_addr,
-    output word_t data_sram_wdata,
-    input word_t data_sram_rdata,
+    output logic inst_req, data_req,
+    output logic inst_wr, data_wr,
+    output logic [1:0]inst_size, data_size,
+    output word_t inst_addr, data_addr,
+    output word_t inst_wdata, data_wdata,
+    input word_t inst_rdata, data_rdata,
+    input logic inst_addr_ok, data_addr_ok,
+    input logic inst_data_ok, data_data_ok
 
     //debug
     output word_t debug_wb_pc,
@@ -62,59 +59,78 @@ module mycpu_top(
 endmodule
 
 
-// axi
-// module mycpu_top (
-//         .ext_int   (6'd0          ),   //high active
+axi
+module mycpu_top (
+    input logic[5:0] ext_int,  //high active
 
-//     .aclk      (cpu_clk       ),
-//     .aresetn   (cpu_resetn    ),   //low active
+    input logic aclk,
+    input logic aresetn,   //low active
 
-//     .arid      (cpu_arid      ),
-//     .araddr    (cpu_araddr    ),
-//     .arlen     (cpu_arlen     ),
-//     .arsize    (cpu_arsize    ),
-//     .arburst   (cpu_arburst   ),
-//     .arlock    (cpu_arlock    ),
-//     .arcache   (cpu_arcache   ),
-//     .arprot    (cpu_arprot    ),
-//     .arvalid   (cpu_arvalid   ),
-//     .arready,
-                
-//     .rid,
-//     .rdata,
-//     .rresp,
-//     .rlast,
-//     .rvalid,
-//     .rready,
-               
-//     .awid,
-//     .awaddr,
-//     .awlen,
-//     .awsize,
-//     .awburst,
-//     .awlock,
-//     .awcache,
-//     .awprot,
-//     .awvalid,
-//     .awready,
-    
-//     .wid,
-//     .wdata,
-//     .wstrb,
-//     .wlast,
-//     .wvalid,
-//     .wready,
-    
-//     .bid,
-//     .bresp,
-//     .bvalid,
-//     .bready,
+    output logic [3:0] arid,
+    output logic [31:0] araddr,
+    output logic [3:0] arlen,
+    output logic [2 :0] arsize ,
+    output logic [1 :0] arburst,
+    output logic [1 :0] arlock ,
+    output logic [3 :0] arcache,
+    output logic [2 :0] arprot ,
+    output logic        arvalid,
+    input logic        arready,
+    input logic [3 :0] rid    ,
+    input logic [31:0] rdata  ,
+    input logic [1 :0] rresp  ,
+    input logic        rlast  ,
+    input logic        rvalid ,
+    output logic        rready ,
+    output logic [3 :0] awid   ,
+    output logic [31:0] awaddr ,
+    output logic [3 :0] awlen  ,
+    output logic [2 :0] awsize ,
+    output logic [1 :0] awburst,
+    output logic [1 :0] awlock ,
+    output logic [3 :0] awcache,
+    output logic [2 :0] awprot ,
+    output logic        awvalid,
+    input logic        awready,
+    output logic [3 :0] wid    ,
+    output logic [31:0] wdata  ,
+    output logic [3 :0] wstrb  ,
+    output logic        wlast  ,
+    output logic        wvalid ,
+    input logic        wready ,
+    input logic [3 :0] bid    ,
+    input logic [1 :0] bresp  ,
+    input logic        bvalid ,
+    output logic        bready ,
 
-//     //debug interface
-//     .debug_wb_pc,
-//     .debug_wb_rf_wen,
-//     .debug_wb_rf_wnum,
-//     .debug_wb_rf_wdata
-// );
-    
-// endmodule
+    //debug interface
+    output logic[31:0] debug_wb_pc,
+    output logic[3:0] debug_wb_rf_wen,
+    output creg_addr_t debug_wb_rf_wnum,
+    output word_t debug_wb_rf_wdata
+);
+    logic inst_req, data_req;
+    logic inst_wr, data_wr;
+    logic [1:0]inst_size, data_size;
+    word_t inst_addr, data_addr;
+    word_t inst_wdata, data_wdata;
+    word_t inst_rdata, data_rdata;
+    logic inst_addr_ok, data_addr_ok;
+    logic inst_data_ok, data_data_ok;
+    cpu_axi_interface cpu_axi_interface(
+        .clk(aclk), .resetn(aresetn),
+        .inst_req, .inst_wr, .inst_size, .inst_addr, .inst_wdata, .inst_rdata, .inst_addr_ok, .inst_data_ok,
+        .data_req, .data_wr, .data_size, .data_addr, .data_wdata, .data_rdata, .data_addr_ok, .data_data_ok,
+        .arid, .araddr, .arlen, .arsize, .arburst, .arlock, .arcache, .arprot, .arvalid , .arready,
+        .rid, .rdata, .rresp, .rlast, .rvalid, .rready, 
+        .awid, .awaddr, .awlen, .awsize, .awburst, .awlock, .awcache, .awprot, .awvalid, .awready,
+        .wid, .wdata, .wstrb, .wlast, .wvalid, .wready, 
+        .bid, .bresp, .bvalid, .bready
+    );
+    mucpu mycpu(
+        .clk(aclk), .resetn(aresetn), .ext_int,
+        .inst_req, .inst_wr, .inst_size, .inst_addr, .inst_wdata, .inst_rdata, .inst_addr_ok, .inst_data_ok,
+        .data_req, .data_wr, .data_size, .data_addr, .data_wdata, .data_rdata, .data_addr_ok, .data_data_ok,
+        .debug_wb_pc, .debug_wb_rf_wen, .debug_wb_rf_wnum, .debug_wb_rf_wdata
+    );
+endmodule
