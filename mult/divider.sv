9f8b0ca28d164db3bc124e903c144654fa3f2dbf
15711657500 <15711657500@163.com>
2020-07-13 21:23:34 +0800

    update divider. to be debugged

--- mult/divider.sv
+++ mult/divider.sv
@@ -1,5 +1,6 @@
 // unsigned
 `include "mips.svh"
+`include "divider.svh"
 module divider(
     input logic clk, reset,
     input logic valid,
@@ -8,23 +9,32 @@ module divider(
     output logic ok
 );
     logic [4:0] shift_left;
-    divide_data_t div1, div2, div3, div4;
-    divide_data_t div1_new, div2_new, div3_new, div4_new;
+    divide_data_t [17:1]div;
+    divide_data_t [16:0]div_new;
     always_ff @(posedge clk) begin
         if (reset) begin
-            {div2, div3, div4} <= '0;
+            div <= '0;
         end else begin
-            {div2, div3, div4} <= {div1_new, div2_new, div3_new};
+            div[17:1] <= div_new[16:0];
         end
     end
     // stage 1
-    assign div1_new.ok = valid;
-    bitcount bitcount(.ina(a), .inb(b), .outpa(div1_new.PA), .outb(div1_new.B));
-    assign div1_new.R =
+    assign div_new[0].ok = valid;
+    assign div_new[0].Q = '0;
+    divide_initial divide_initial(.ina(a), .inb(b), .outpa(div_new[0].PA), .outb(div_new[0].B), .shiftnum(div_new[0].shiftnum));
 
+    genvar i;
+    generate
+        for (i=1; i<=16; i++) begin
+            divide_process divide_process(.in(div[i]), .out(div_new[i]));
+        end
+    endgenerate
 
-
-    // end
+    // output
+    assign ok = div[17].ok;
+    assign out = div[17].PA[64] ? 
+                {{(div[17].PA[63:32] + div[17].B) >> div[17].shiftnum}, {div[17].Q - 32'b1} } // negetive
+               :{{div[17].PA[63:32] >> div[17].shiftnum}, div[17].Q };
 endmodule
 
 
@@ -32,10 +42,392 @@ module divide_process (
     input divide_data_t in,
     output divide_data_t out
 );
-    
+    // quotient_bit_t [7:0]q;
+    // genvar i;
+    // generate
+    //     for (i=0; i<8; ++i) begin
+    //         divide_table divide_table(.b(in.B[30:28]), .pa(in.PA[64:59]), .q(q[i]));
+    //     end
+    // endgenerate
+    quotient_bit_t q;
+    divide_table divide_table(.b(in.B[30:28]), .pa(in.PA[64:59]), .q(q));
+    always_comb begin
+        case (q)
+            TWO_P: begin
+                out.Q = {in.Q[29:0], 2'b10};
+                out.PA[64:32] = in.PA[64:32] - {in.B[31:0], 1'b0};
+            end
+            ONE_P: begin
+                out.Q = {in.Q[29:0], 2'b01};
+                out.PA[64:32] = in.PA[64:32] - {1'b0, in.B[31:0]};
+            end
+            ZERO_P: begin
+                out.Q = {in.Q[29:0], 2'b00};
+                out.PA[64:32] = in.PA[64:32];
+            end
+            ONE_N: begin
+                out.Q = {{in.Q[29:0] - 30'b01}, 2'b11};
+                out.PA[64:32] = in.PA[64:32] + {1'b0, in.B[31:0]};
+            end
+            TWO_N: begin
+                out.Q = {{in.Q[29:0] - 30'b01}, 2'b10};
+                out.PA[64:32] = in.PA[64:32] + {in.B[31:0], 1'b0};
+            end
+            default: begin
+                out.PA[64:32] = '0;
+                out.Q = '0;
+            end
+        endcase
+    end
+    assign out.ok = in.ok;
+    assign out.shiftnum = in.shiftnum;
+    assign out.B = in.B;
+    assign out.PA[31:0] = in.PA[31:0];
 endmodule
 
-module divinitial (
+module divide_table (
+    input logic [2:0] b, // b[30:28]
+    input logic [5:0] pa,
+    output quotient_bit_t q
+);
+    always_comb begin
+        case (b)
+            3'b000:begin
+                case (pa)
+                    6'b110100: q = TWO_N;
+                    6'b110101: q = TWO_N;
+                    6'b110110: q = TWO_N;
+                    6'b110111: q = TWO_N;
+                    6'b111000: q = TWO_N;
+                    6'b111001: q = TWO_N;
+                    6'b111010: q = ONE_N;
+                    6'b111011: q = ONE_N;
+                    6'b111100: q = ONE_N;
+                    6'b111101: q = ONE_N;
+                    6'b111110: q = ZERO_P;
+                    6'b111111: q = ZERO_P;
+                    6'b000000: q = ZERO_P;
+                    6'b000001: q = ZERO_P;
+                    6'b000010: q = ONE_P;
+                    6'b000011: q = ONE_P;
+                    6'b000100: q = ONE_P;
+                    6'b000101: q = ONE_P;
+                    6'b000110: q = TWO_P;
+                    6'b000111: q = TWO_P;
+                    6'b001000: q = TWO_P;
+                    6'b001001: q = TWO_P;
+                    6'b001010: q = TWO_P;
+                    6'b001011: q = TWO_P;
+                    default: begin
+                        q = WRONG_N;
+                    end
+                endcase
+            end
+            3'b001:begin
+                case (pa)
+                    6'b110010: q = TWO_N;
+                    6'b110011: q = TWO_N;
+                    6'b110100: q = TWO_N;
+                    6'b110101: q = TWO_N;
+                    6'b110110: q = TWO_N;
+                    6'b110111: q = TWO_N;
+                    6'b111000: q = TWO_N;
+                    6'b111001: q = ONE_N;
+                    6'b111010: q = ONE_N;
+                    6'b111011: q = ONE_N;
+                    6'b111100: q = ONE_N;
+                    6'b111101: q = ONE_N;
+                    6'b111110: q = ZERO_P;
+                    6'b111111: q = ZERO_P;
+                    6'b000000: q = ZERO_P;
+                    6'b000001: q = ZERO_P;
+                    6'b000010: q = ZERO_P;
+                    6'b000011: q = ONE_P;
+                    6'b000100: q = ONE_P;
+                    6'b000101: q = ONE_P;
+                    6'b000110: q = ONE_P;
+                    6'b000111: q = TWO_P;
+                    6'b001000: q = TWO_P;
+                    6'b001001: q = TWO_P;
+                    6'b001010: q = TWO_P;
+                    6'b001011: q = TWO_P;
+                    6'b001100: q = TWO_P;
+                    6'b001101: q = TWO_P;
+                    default: begin
+                        q = WRONG_N;
+                    end
+                endcase
+            end
+            3'b010:begin
+                case (pa)
+                    6'b110001: q = TWO_N;
+                    6'b110010: q = TWO_N;
+                    6'b110011: q = TWO_N;
+                    6'b110100: q = TWO_N;
+                    6'b110101: q = TWO_N;
+                    6'b110110: q = TWO_N;
+                    6'b110111: q = TWO_N;
+                    6'b111000: q = ONE_N;
+                    6'b111001: q = ONE_N;
+                    6'b111010: q = ONE_N;
+                    6'b111011: q = ONE_N;
+                    6'b111100: q = ONE_N;
+                    6'b111101: q = ONE_N;
+                    6'b111110: q = ZERO_P;
+                    6'b111111: q = ZERO_P;
+                    6'b000000: q = ZERO_P;
+                    6'b000001: q = ZERO_P;
+                    6'b000010: q = ZERO_P;
+                    6'b000011: q = ONE_P;
+                    6'b000100: q = ONE_P;
+                    6'b000101: q = ONE_P;
+                    6'b000110: q = ONE_P;
+                    6'b000111: q = ONE_P;
+                    6'b001000: q = TWO_P;
+                    6'b001001: q = TWO_P;
+                    6'b001010: q = TWO_P;
+                    6'b001011: q = TWO_P;
+                    6'b001100: q = TWO_P;
+                    6'b001101: q = TWO_P;
+                    6'b001110: q = TWO_P;
+                    default: begin
+                        q = WRONG_N;
+                    end
+                endcase
+            end
+            3'b011:begin
+                case (pa)
+                    6'b110000: q = TWO_N;
+                    6'b110001: q = TWO_N;
+                    6'b110010: q = TWO_N;
+                    6'b110011: q = TWO_N;
+                    6'b110100: q = TWO_N;
+                    6'b110101: q = TWO_N;
+                    6'b110110: q = TWO_N;
+                    6'b110111: q = TWO_N;
+                    6'b111000: q = ONE_N;
+                    6'b111001: q = ONE_N;
+                    6'b111010: q = ONE_N;
+                    6'b111011: q = ONE_N;
+                    6'b111100: q = ONE_N;
+                    6'b111101: q = ONE_N;
+                    6'b111110: q = ZERO_P;
+                    6'b111111: q = ZERO_P;
+                    6'b000000: q = ZERO_P;
+                    6'b000001: q = ZERO_P;
+                    6'b000010: q = ZERO_P;
+                    6'b000011: q = ONE_P;
+                    6'b000100: q = ONE_P;
+                    6'b000101: q = ONE_P;
+                    6'b000110: q = ONE_P;
+                    6'b000111: q = ONE_P;
+                    6'b001000: q = ONE_P;
+                    6'b001001: q = TWO_P;
+                    6'b001010: q = TWO_P;
+                    6'b001011: q = TWO_P;
+                    6'b001100: q = TWO_P;
+                    6'b001101: q = TWO_P;
+                    6'b001110: q = TWO_P;
+                    6'b001111: q = TWO_P;
+                    default: begin
+                        q = WRONG_N;
+                    end
+                endcase
+            end
+            3'b100:begin
+                case (pa)
+                    6'b101110: q = TWO_N;
+                    6'b101111: q = TWO_N;
+                    6'b110000: q = TWO_N;
+                    6'b110001: q = TWO_N;
+                    6'b110010: q = TWO_N;
+                    6'b110011: q = TWO_N;
+                    6'b110100: q = TWO_N;
+                    6'b110101: q = TWO_N;
+                    6'b110110: q = TWO_N;
+                    6'b110111: q = ONE_N;
+                    6'b111000: q = ONE_N;
+                    6'b111001: q = ONE_N;
+                    6'b111010: q = ONE_N;
+                    6'b111011: q = ONE_N;
+                    6'b111100: q = ONE_N;
+                    6'b111101: q = ZERO_P;
+                    6'b111110: q = ZERO_P;
+                    6'b111111: q = ZERO_P;
+                    6'b000000: q = ZERO_P;
+                    6'b000001: q = ZERO_P;
+                    6'b000010: q = ZERO_P;
+                    6'b000011: q = ZERO_P;
+                    6'b000100: q = ONE_P;
+                    6'b000101: q = ONE_P;
+                    6'b000110: q = ONE_P;
+                    6'b000111: q = ONE_P;
+                    6'b001000: q = ONE_P;
+                    6'b001001: q = ONE_P;
+                    6'b001010: q = TWO_P;
+                    6'b001011: q = TWO_P;
+                    6'b001100: q = TWO_P;
+                    6'b001101: q = TWO_P;
+                    6'b001110: q = TWO_P;
+                    6'b001111: q = TWO_P;
+                    6'b010000: q = TWO_P;
+                    6'b010001: q = TWO_P;
+                    default: begin
+                        q = WRONG_N;
+                    end
+                endcase
+            end
+            3'b101:begin
+                case (pa)
+                    6'b101101: q = TWO_N;
+                    6'b101110: q = TWO_N;
+                    6'b101111: q = TWO_N;
+                    6'b110000: q = TWO_N;
+                    6'b110001: q = TWO_N;
+                    6'b110010: q = TWO_N;
+                    6'b110011: q = TWO_N;
+                    6'b110100: q = TWO_N;
+                    6'b110101: q = TWO_N;
+                    6'b110110: q = ONE_N;
+                    6'b110111: q = ONE_N;
+                    6'b111000: q = ONE_N;
+                    6'b111001: q = ONE_N;
+                    6'b111010: q = ONE_N;
+                    6'b111011: q = ONE_N;
+                    6'b111100: q = ONE_N;
+                    6'b111101: q = ZERO_P;
+                    6'b111110: q = ZERO_P;
+                    6'b111111: q = ZERO_P;
+                    6'b000000: q = ZERO_P;
+                    6'b000001: q = ZERO_P;
+                    6'b000010: q = ZERO_P;
+                    6'b000011: q = ZERO_P;
+                    6'b000100: q = ONE_P;
+                    6'b000101: q = ONE_P;
+                    6'b000110: q = ONE_P;
+                    6'b000111: q = ONE_P;
+                    6'b001000: q = ONE_P;
+                    6'b001001: q = ONE_P;
+                    6'b001010: q = TWO_P;
+                    6'b001011: q = TWO_P;
+                    6'b001100: q = TWO_P;
+                    6'b001101: q = TWO_P;
+                    6'b001110: q = TWO_P;
+                    6'b001111: q = TWO_P;
+                    6'b010000: q = TWO_P;
+                    6'b010001: q = TWO_P;
+                    6'b010010: q = TWO_P;
+                    default: begin
+                        q = WRONG_N;
+                    end
+                endcase
+            end
+            3'b110:begin
+                case (pa)
+                    6'b101100: q = TWO_N;
+                    6'b101101: q = TWO_N;
+                    6'b101110: q = TWO_N;
+                    6'b101111: q = TWO_N;
+                    6'b110000: q = TWO_N;
+                    6'b110001: q = TWO_N;
+                    6'b110010: q = TWO_N;
+                    6'b110011: q = TWO_N;
+                    6'b110100: q = TWO_N;
+                    6'b110101: q = TWO_N;
+                    6'b110110: q = ONE_N;
+                    6'b110111: q = ONE_N;
+                    6'b111000: q = ONE_N;
+                    6'b111001: q = ONE_N;
+                    6'b111010: q = ONE_N;
+                    6'b111011: q = ONE_N;
+                    6'b111100: q = ONE_N;
+                    6'b111101: q = ZERO_P;
+                    6'b111110: q = ZERO_P;
+                    6'b111111: q = ZERO_P;
+                    6'b000000: q = ZERO_P;
+                    6'b000001: q = ZERO_P;
+                    6'b000010: q = ZERO_P;
+                    6'b000011: q = ZERO_P;
+                    6'b000100: q = ONE_P;
+                    6'b000101: q = ONE_P;
+                    6'b000110: q = ONE_P;
+                    6'b000111: q = ONE_P;
+                    6'b001000: q = ONE_P;
+                    6'b001001: q = ONE_P;
+                    6'b001010: q = ONE_P;
+                    6'b001011: q = TWO_P;
+                    6'b001100: q = TWO_P;
+                    6'b001101: q = TWO_P;
+                    6'b001110: q = TWO_P;
+                    6'b001111: q = TWO_P;
+                    6'b010000: q = TWO_P;
+                    6'b010001: q = TWO_P;
+                    6'b010010: q = TWO_P;
+                    6'b010011: q = TWO_P;
+                    default: begin
+                        q = WRONG_N;
+                    end
+                endcase
+            end
+            3'b111:begin
+                case (pa)
+                    6'b101010: q = TWO_N;
+                    6'b101011: q = TWO_N;
+                    6'b101100: q = TWO_N;
+                    6'b101101: q = TWO_N;
+                    6'b101110: q = TWO_N;
+                    6'b101111: q = TWO_N;
+                    6'b110000: q = TWO_N;
+                    6'b110001: q = TWO_N;
+                    6'b110010: q = TWO_N;
+                    6'b110011: q = TWO_N;
+                    6'b110100: q = TWO_N;
+                    6'b110101: q = ONE_N;
+                    6'b110110: q = ONE_N;
+                    6'b110111: q = ONE_N;
+                    6'b111000: q = ONE_N;
+                    6'b111001: q = ONE_N;
+                    6'b111010: q = ONE_N;
+                    6'b111011: q = ONE_N;
+                    6'b111100: q = ONE_N;
+                    6'b111101: q = ZERO_P;
+                    6'b111110: q = ZERO_P;
+                    6'b111111: q = ZERO_P;
+                    6'b000000: q = ZERO_P;
+                    6'b000001: q = ZERO_P;
+                    6'b000010: q = ZERO_P;
+                    6'b000011: q = ZERO_P;
+                    6'b000100: q = ZERO_P;
+                    6'b000101: q = ONE_P;
+                    6'b000110: q = ONE_P;
+                    6'b000111: q = ONE_P;
+                    6'b001000: q = ONE_P;
+                    6'b001001: q = ONE_P;
+                    6'b001010: q = ONE_P;
+                    6'b001011: q = ONE_P;
+                    6'b001100: q = TWO_P;
+                    6'b001101: q = TWO_P;
+                    6'b001110: q = TWO_P;
+                    6'b001111: q = TWO_P;
+                    6'b010000: q = TWO_P;
+                    6'b010001: q = TWO_P;
+                    6'b010010: q = TWO_P;
+                    6'b010011: q = TWO_P;
+                    6'b010100: q = TWO_P;
+                    6'b010101: q = TWO_P; 
+                    default: begin
+                        q = WRONG_N;
+                    end
+                endcase
+            end
+            default: begin
+                q = WRONG_N;
+            end
+        endcase
+    end
+endmodule
+
+module divide_initial (
     input word_t ina,
     input word_t inb,
     output logic [64:0] outpa,
@@ -46,7 +438,7 @@ module divinitial (
     assign inpa = {33'b0, ina};
     always_comb begin
         priority case (1'b1)
-            inb[31] : {outpa, outb, shiftnum} = {{inpa[64:0], 0'b0}, {inb[31:0], 0'b0}, 5'd0};
+            inb[31] : {outpa, outb, shiftnum} = {inpa[64:0], inb[31:0], 5'd0};
             inb[30] : {outpa, outb, shiftnum} = {{inpa[63:0], 1'b0}, {inb[30:0], 1'b0}, 5'd1};
             inb[29] : {outpa, outb, shiftnum} = {{inpa[62:0], 2'b0}, {inb[29:0], 2'b0}, 5'd2};
             inb[28] : {outpa, outb, shiftnum} = {{inpa[61:0], 3'b0}, {inb[28:0], 3'b0}, 5'd3};
@@ -81,7 +473,7 @@ module divinitial (
 
 
             default: begin
-                {outpa, outb, shiftmum} = '0;
+                {outpa, outb, shiftnum} = '0;
             end
         endcase
     end
