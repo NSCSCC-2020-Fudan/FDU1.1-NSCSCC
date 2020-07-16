4487f0e68e871d90dda2f8e44ec62379cecc2c8a
15711657500 <15711657500@163.com>
2020-07-16 17:25:05 +0800

    update rat

--- code/renaming/rat.sv
+++ code/renaming/rat.sv
@@ -8,7 +8,8 @@ module rat (
     table_t mapping_table;
 
     // write
-
+    w_req_t [WRITE_NUM-1:0] write;
     // read
+    r_req_t [READ_NUM-1:0] read;
     
 endmodule
--- code/renaming/rat_pkg.sv
+++ code/renaming/rat_pkg.sv
@@ -3,12 +3,21 @@
 package rat_pkg;
     import common::*;
     //  Group: Parameters
-    
+    parameter READ_NUM = 12;
+    parameter WRITE_NUM = 4;
 
     //  Group: Typedefs
     typedef struct packed {
         preg_addr_t preg_id;
     } entry_t;
     typedef entry_t [AREG_NUM-1:0] table_t; 
-    
+    typedef struct packed {
+        logic wen;
+        logic is_dst;
+        areg_addr_t areg_id;
+        preg_addr_t preg_id;
+    } w_req_t;
+    typedef struct packed {
+        areg_addr_t areg_id;
+    } r_req_t;
 endpackage: rat_pkg
