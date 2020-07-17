f4b0c07c99d64455c2e3a5359457c629d78e53f4
15711657500 <15711657500@163.com>
2020-07-17 16:06:59 +0800

    update renaming

--- code/renaming/rat.sv
+++ code/renaming/rat.sv
@@ -8,8 +8,44 @@ module rat (
     table_t mapping_table;
 
     // write
-    w_req_t [WRITE_NUM-1:0] write;
+    w_req_t [WRITE_NUM-1:0] w_req;
+    logic [WRITE_NUM-1:0] wen;
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            mapping_table <= '0;
+        end
+        else begin
+            for (int j=0; j<WRITE_NUM; j++) begin
+                if (w_req[j].req) begin
+                    pass
+                end
+            end
+        end
+    end
+    assign wen[WRITE_NUM-1] = (w_req[WRITE_NUM].id != '0);
+    always_comb begin
+        for (int i=0; i<WRITE_NUM-1; i++) begin
+            if (w_req[i].id != 0) begin
+                wen[i] = 1'b1;
+                for (int j=i+1; j<WRITE_NUM; j++) begin
+                    if (w_req[i].id == w_req[j].id) begin
+                        wen[i] = 1'b0;
+                        break;
+                    end
+                end
+            end else begin
+                wen[i] = 1'b0;
+            end
+        end
+    end
     // read
-    r_req_t [READ_NUM-1:0] read;
-    
+    r_req_t [READ_NUM-1:0] r_req;
+    r_resp_t [READ_NUM-1:0] r_resp;
+    for (int i=0; i<READ_NUM; i++) begin
+        assign r_resp[i].preg_id = mapping_table[r_req[i].areg_id];
+    end
+    // r0 is always 0
+    assign mapping_table[0].preg_id = '0;
+
+
 endmodule
---
+++ code/renaming/renaming_pkg.sv
@@ -0,0 +1,11 @@
+//  Package: renaming_pkg
+//
+package renaming_pkg;
+    //  Group: Parameters
+    
+
+    //  Group: Typedefs
+    
+
+    
+endpackage: renaming_pkg
