412d97038a0fb95012f74790260030dcc73309c1
15711657500 <15711657500@163.com>
2020-07-19 19:16:17 +0800

    start execute

---
+++ code/commit/exc_pkg.sv
@@ -0,0 +1,11 @@
+//  Package: exc_pkg
+//
+package exc_pkg;
+    //  Group: Parameters
+    
+
+    //  Group: Typedefs
+    typedef enum logic[4:0] { NONE, INTERUPT, INSTR, LOAD, SAVE, BP, SYS } exception_info_t;
+
+    
+endpackage: exc_pkg
