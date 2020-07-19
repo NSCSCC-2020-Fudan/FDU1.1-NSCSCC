cec0ca2d5a973c8d3213b7ab9a9af74a7d5e9246
15711657500 <15711657500@163.com>
2020-07-19 19:16:17 +0800

    start execute

---
+++ code/execute/agu.sv
@@ -0,0 +1,10 @@
+// address generate unit
+module agu 
+    import common::*;
+    (
+    
+);
+    vaddr_t addr;
+    word_t data;
+    
+endmodule
---
+++ code/execute/alu.sv
@@ -0,0 +1,72 @@
+module alu 
+    import common::*;(
+    input word_t a, b,
+    input alufunc_t alufunc,
+    output word_t c,
+    output logic exception_of
+);
+    shamt_t shamt;
+    assign shamt = a[4:0];
+    logic [32:0]temp;
+    always_comb begin
+        exception_of = 0;
+        temp = '0;
+        case (alufunc)
+            ALU_AND: begin
+                c = a & b;
+            end
+            ALU_ADD: begin
+                c = a + b;
+                temp = {a[31], a} + {b[31], b};
+                exception_of = (temp[32] != temp[31]);
+            end
+            ALU_OR: begin
+                c = a | b;
+            end
+            ALU_SLL: begin
+                c = b << shamt;
+            end
+            ALU_SRL: begin
+                c = b >> shamt; 
+            end
+            ALU_SRA: begin
+                c = signed'(b) >>> shamt;
+            end
+            ALU_SUB: begin
+                c = a - b;
+                temp = {a[31], a} - {b[31], b};
+                exception_of = (temp[32] != temp[31]);
+            end
+            ALU_SLT: begin
+                c = (signed'(a) < signed'(b)) ? 32'b1 : 32'b0; 
+            end
+            ALU_NOR: begin
+                c = ~(a | b);
+            end
+            ALU_XOR: begin
+                c = a ^ b;
+            end
+            ALU_ADDU: begin
+                c = a + b;
+            end
+            ALU_SUBU: begin
+                c = a - b;
+            end
+            ALU_SLTU: begin
+                c = (a < b) ? 32'b1 : 32'b0;
+            end
+            ALU_PASSA: begin
+                c = a;
+            end
+            ALU_LUI : begin
+                c = {b[15:0], 16'b0};
+            end
+            ALU_PASSB: begin
+                c = b;
+            end
+            default: begin
+                c = '0;
+            end
+        endcase
+    end
+endmodule
