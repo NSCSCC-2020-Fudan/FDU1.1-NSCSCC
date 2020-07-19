af1f168d259c857ac32157ce6beb2b29025a97ae
15711657500 <15711657500@163.com>
2020-07-19 13:09:02 +0800

    update issue

--- code/issue/issue_pkg.sv
+++ code/issue/issue_pkg.sv
@@ -1,12 +1,14 @@
 //  Package: issue_pkg
 //
 package issue_pkg;
+    import common::*;
     //  Group: Parameters
-    parameter ISSUE_WIDTH = 6; // maximum issue
-    parameter MACHINE_WIDTH = 2; // maximum decode&renaming
+
 
     //  Group: Typedefs
-    
+    typedef struct packed {
+        
+    } struct_name;
 
     
 endpackage: issue_pkg
--- code/issue/queue_pkg.sv
+++ code/issue/queue_pkg.sv
@@ -3,8 +3,10 @@
 package queue_pkg;
     import common::*;
     //  Group: Parameters
-    parameter QUEUE_LEN = 32;
-
+    parameter ALU_QUEUE_LEN = 32;
+    parameter MEM_QUEUE_LEN = 32;
+    parameter BRANCH_QUEUE_LEN = 16;
+    parameter MULT_QUEUE_LEN = 8;
     //  Group: Typedefs
     typedef struct packed {
         logic valid;
@@ -14,7 +16,24 @@ package queue_pkg;
     typedef struct packed {
         preg_addr_t dst;
         src_data_t src1, src2;
+        control_t ctl;
     } entry_t;
-    typedef entry_t[QUEUE_LEN-1:0] issue_queue_t;
-    
+    typedef entry_t[ALU_QUEUE_LEN-1:0] alu_queue_t;
+    typedef entry_t[MEM_QUEUE_LEN-1:0] mem_queue_t;
+    typedef entry_t[BRANCH_QUEUE_LEN-1:0] branch_queue_t;
+    typedef entry_t[MULT_QUEUE_LEN-1:0] mult_queue_t;
+    typedef logic[$clog2(ALU_QUEUE_LEN):0] alu_queue_ptr_t;
+    typedef logic[$clog2(MEM_QUEUE_LEN):0] mem_queue_ptr_t;
+    typedef logic[$clog2(BRANCH_QUEUE_LEN):0] branch_queue_ptr_t;
+    typedef logic[$clog2(MULT_QUEUE_LEN):0] mult_queue_ptr_t;
+
+    // write
+    typedef enum logic[1:0] { ALU, MEM, BRANCH, MULT } entry_type_t;
+    typedef struct packed {
+        logic valid;
+        entry_type_t entry_type;
+        entry_t entry;
+    } write_req_t;
+
+    // read
 endpackage: queue_pkg
