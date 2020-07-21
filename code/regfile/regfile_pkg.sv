7362d0494b42c4b4c7e01f62bbb8eaa9175b3029
15711657500 <15711657500@163.com>
2020-07-21 15:57:34 +0800

    add arf

--- code/regfile/regfile_pkg.sv
+++ code/regfile/regfile_pkg.sv
@@ -1,10 +1,22 @@
 //  Package: regfile_pkg
 //
 package regfile_pkg;
+    import common::*;
     //  Group: Parameters
-
+    parameter READ_PORTS = 1;
+    parameter WRITE_PORTS = 1;
     //  Group: Typedefs
-    
-
-    
+    typedef enum logic { READ_FIRST, WRITE_FIRST } mode_t;
+    typedef struct packed {
+        areg_addr_t id;
+        mode_t mode;
+    } read_req_t;
+    typedef struct packed {
+        logic valid;
+        areg_addr_t id;
+        word_t data;
+    } write_req_t;
+    typedef struct packed {
+        word_t data;
+    } read_resp_t;
 endpackage: regfile_pkg
