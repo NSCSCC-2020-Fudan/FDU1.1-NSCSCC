1e6d88486ad8edb2c93d1d2e160baa219a83a649
15711657500 <15711657500@163.com>
2020-07-21 15:57:34 +0800

    add arf

--- code/regfile/arf.sv
+++ code/regfile/arf.sv
@@ -1,9 +1,36 @@
 module arf 
 	import common::*;
+	import regfile_pkg::*;
 	(
     
 );
-    
-    word_t[AREG_NUM-1:0] regfile;
-    
+    word_t[AREG_NUM-1:0] regfile, regfile_new;
+	read_req_t [READ_PORTS-1:0]reads;
+	write_req_t [WRITE_PORTS-1:0]writes;
+	read_resp_t [READ_PORTS-1:0]read_resps;
+	// write
+	always_comb begin
+		regfile_new = regfile;
+		for (int i=0; i<WRITE_PORTS; i++) begin
+			if (writes[i].valid) begin
+				regfile_new[writes[i].id] = regfile_new.data;
+			end		
+		end
+	end
+
+	// read
+	always_comb begin
+		for (int i=0; i<READ_PORTS; i++) begin
+			read_resps[i].data = reads[i].mode == READ_FIRST ? 
+							     regfile[reads[i].id] : regfile_new[reads[i].id];
+		end
+	end
+
+	always_ff @(posedge clk) begin
+		if (~resetn) begin
+			regfile <= '0;
+		end else begin
+			regfile <= regfile_new;
+		end
+	end
 endmodule
