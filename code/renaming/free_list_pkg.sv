b1fd751bcf379884e53c9d72dfa9ef9cbed6c0ee
15711657500 <15711657500@163.com>
2020-07-22 11:24:44 +0800

    adding ports of renaming

--- code/renaming/free_list_pkg.sv
+++ code/renaming/free_list_pkg.sv
@@ -6,11 +6,12 @@ package free_list_pkg;
     parameter READ_PORTS = 1;
     parameter WRITE_PORTS = 1;
     parameter RELEASE_PORTS = 1;
+    parameter LIST_LEN = PREG_NUM;
     //  Group: Typedefs
     typedef struct packed {
         logic busy;
     } entry_t;
-    typedef entry_t[PREG_NUM-1:0] free_list_t;
+    typedef entry_t[LIST_LEN-1:0] free_list_t;
     typedef struct packed {
         logic valid;
         preg_addr_t id;
