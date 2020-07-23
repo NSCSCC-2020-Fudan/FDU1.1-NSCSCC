e9ae84861c2e8d2a7f39da6f72d24f72889b6da0
riteme <riteme@qq.com>
2020-07-23 23:28:42 +0800

    Attempt to fix SRAMxToInstrBus issue.

--- cache/src/util/SRAMxToInstrBus.sv
+++ cache/src/util/SRAMxToInstrBus.sv
@@ -14,7 +14,10 @@ module SRAMxToInstrBus(
     assign ibus_req.addr      = sramx_req.addr;
     assign sramx_resp.addr_ok = ibus_resp.addr_ok;
     assign sramx_resp.data_ok = ibus_resp.data_ok;
-    assign sramx_resp.rdata   = ibus_resp.data[
+
+    typedef logic [IBUS_DATA_WIDTH:1] ibus_view_t;
+    ibus_view_t data_view = ibus_resp.data;
+    assign sramx_resp.rdata   = data_view[
         {ibus_resp.index, {SRAMX_DATA_BITS{1'b0}}} -: SRAMX_DATA_WIDTH
     ];
 
