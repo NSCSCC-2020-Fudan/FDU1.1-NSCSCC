4464f00c487bafd3413a22d3ed4a62322cb908eb
15711657500 <15711657500@163.com>
2020-07-24 14:00:13 +0800

    update decoder

--- code/regfile/prf.sv
+++ code/regfile/prf.sv
@@ -1,18 +1,18 @@
-module arf 
+module prf 
 	import common::*;
 	import regfile_pkg::*;
 	(
     
 );
-    word_t[AREG_NUM-1:0] regfile, regfile_new;
-	read_req_t [AREG_READ_PORTS-1:0]reads;
-	write_req_t [AREG_WRITE_PORTS-1:0]writes;
-	read_resp_t [AREG_READ_PORTS-1:0]read_resps;
+    word_t[PREG_NUM-1:0] regfile, regfile_new;
+	read_req_t [PREG_READ_PORTS-1:0]reads;
+	write_req_t [PREG_WRITE_PORTS-1:0]writes;
+	read_resp_t [PREG_READ_PORTS-1:0]read_resps;
 	// write
 	always_comb begin
 		regfile_new = regfile;
-		for (int i=1; i<AREG_NUM; i++) begin
-			for (int j=0; j<AREG_WRITE_PORTS; j++) begin
+		for (int i=1; i<PREG_NUM; i++) begin
+			for (int j=0; j<PREG_WRITE_PORTS; j++) begin
 				if (writes[j].valid && writes[j].id == i) begin
 					regfile_new[i] = writes[j].data;
 				end
@@ -21,7 +21,7 @@ module arf
 	end
 
 	// read		
-	for (genvar i=0; i<AREG_READ_PORTS; i++) 
+	for (genvar i=0; i<PREG_READ_PORTS; i++) 
 		assign read_resps[i].data = reads[i].mode == READ_FIRST ? 
 									  regfile[reads[i].id] : regfile_new[reads[i].id];
 	
