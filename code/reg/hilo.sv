76bbff60d89b98cffd4807bf8961ffe8307c7ebb
15711657500 <15711657500@163.com>
2020-07-24 10:52:21 +0800

    fix bug of hilo

--- code/reg/hilo.sv
+++ code/reg/hilo.sv
@@ -4,17 +4,26 @@ module hilo (
     input logic clk, reset,
     hilo_intf.hilo ports
 );
-    always_ff @(posedge clk, posedge reset) begin
-        if (reset) begin
-            ports.hi <= '0;
-            ports.lo <= '0;
-        end else begin
-            if (ports.hlwrite.wen_h) begin
-                ports.hi <= ports.hlwrite.wd_h;
-            end
-            if (ports.hlwrite.wen_l) begin
-                ports.lo <= ports.hlwrite.wd_l;
-            end
+    word_t hi, lo, hi_new, lo_new;
+    always_comb begin
+        hi_new = hi;
+        lo_new = lo;
+        if (ports.hlwrite.wen_h) begin
+            hi_new = ports.hlwrite.wd_h;
+        end
+        if (ports.hlwrite.wen_l) begin
+            lo_new = ports.hlwrite.wd_l;
         end
     end
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            hi <= '0;
+            lo <= '0;
+        end else begin
+            hi <= hi_new;
+            lo <= lo_new;
+        end
+    end
+    assign ports.hi = hi_new;
+    assign ports.lo = lo_new;
 endmodule
