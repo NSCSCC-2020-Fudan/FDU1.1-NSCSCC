8f8176f6db7d4e69cdfd1891207b08833b08c708
riteme <riteme@qq.com>
2020-07-29 13:29:16 +0800

    Add "sources.tcl".

---
+++ superscalar_inorder_dual/sources.tcl
@@ -0,0 +1,8 @@
+cd [file dirname [info script]]
+add_files [glob *.sv]
+add_files [glob *.svh]
+add_files [glob bpb/*.sv]
+add_files [glob mult/*.sv]
+add_files [glob mult/*.svh]
+add_files [glob mult/*/*.xci]
+add_files [glob top/*.sv]
