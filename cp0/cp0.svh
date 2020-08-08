9605c4d376c6a4aab2daf9aaf953cbae81eb3e17
15711657500 <15711657500@163.com>
2020-08-08 16:57:19 +0800

    update cp0_structure

--- cp0/cp0.svh
+++ cp0/cp0.svh
@@ -3,6 +3,89 @@
 
 typedef logic [4:0] cp0_addr_t;
 
+parameter TLB_INDEX = 3;
+typedef struct packed {
+    logic P;                    // 31, Probe Failure, R
+    logic [30:TLB_INDEX] zero;  // [30:n], always 0
+    logic [TLB_INDEX-1:0]index; // [n-1:0], TLB index, R/W
+} cp0_index_t;
+
+typedef struct packed {
+    logic [31:TLB_INDEX] zero;
+    logic [TLB_INDEX-1:0] random; // TLB Random Index, R
+                                  // with special upper and lower bound 
+} cp0_random_t;
+
+parameter PABITS = 16;          // physical address bits
+typedef struct packed {
+    logic [31:PABITS - 6] fill;     // always 0, R
+    logic [PABITS-7:6] PFN;         // page frame number, R/W
+    logic [2:0] C;                  // Cacheability and Coherency Attribute of the page R/W
+    logic D;                        // Dirty bit, R/W
+    logic V;                        // Valid bit, R/W
+    logic G;                        // Global bit, R/W
+} cp0_entrylo_t;
+
+typedef struct packed {
+    logic[8:0]PTEbase;              // Page Table Entry, R/W
+
+    logic[18:0]badVPN2;
+    // This field is written by hardware on a TLB exception. 
+    // It contains bits VA 31..13 of the virtual address that caused the exception.
+    // R
+
+    logic[3:0]zero;
+} cp0_context_t;
+
+typedef struct packed {
+    logic[2:0]zero_0;
+    logic[15:0]mask;
+    /*
+    The Mask field is a bit mask in which a “1” bit
+    indicates that the corresponding bit of the vir-
+    tual address should not participate in the TLB
+    match.
+    R/W
+    */
+    logic[12:0]zero_1;
+} cp0_pagemask_t;
+
+typedef struct packed {
+    logic[31:TLB_INDEX]zero;
+    logic[TLB_INDEX-1:0]wired; // TLB wired boundary, R/W
+} cp0_wired_t;
+
+typedef struct packed {
+    logic[18:0]VPN2;
+    /*
+    VA 31..13 of the virtual address (virtual page number / 2).
+    This field is written by hardware on a TLB exception or
+    on a TLB read, and is written by software before a TLB
+    write.
+    R/W
+    */
+    logic[4:0]zero;
+    logic[7:0]ASID;
+    /*
+    Address space identifier. This field is written by hard-
+    ware on a TLB read and by software to establish the cur-
+    rent ASID value for TLB write and against which TLB
+    references match each entry’s TLB ASID field.
+    R/W
+    */
+} cp0_entryhi_t;
+
+typedef struct packed {
+    logic M;        // 31, reset as 1
+    logic [2:0]K23; // [30:28], fixed mapping MMU?, R/W
+    logic [2:0]KU;  // [27:25], fixed mapping MMU?, R/W
+    logic [9:0]zero_0; // [24:10]
+    logic [2:0]MT;  // [9:7] MMU Type, R
+    logic [2:0]zero_1; // [6:4]
+    logic VI;       // 3, Virtual instruction cache, R
+    logic [2:0]K0; // [2:0] Kseg0 cacheability and coherency attribute, R/W
+} cp0_config_t;
+
 typedef struct packed {
     logic BD;           // 31, Branch Delay Slot. Updated only if status.exl is 0. R
     logic TI;           // 30, Timer Interrupt. R
@@ -44,6 +127,7 @@ typedef struct packed {
 } cp0_status_t;
 
 typedef struct packed {
+    word_t config_1; // preset
     word_t 
         desave,     // 31, EJTAG debug exception save register
         errorepc,   // 30, Program counter at last error
@@ -59,26 +143,36 @@ typedef struct packed {
         reserved20, // 20, reserved
         watchhi,    // 19, Watchpoint control
         watchlo,    // 18, Watchpoint address
-        lladdr,     // 17, Load linked address
-        config_,    // 16, Configuration register
+        lladdr;     // 17, Load linked address
+    cp0_config_t
+        config_;    // 16, Configuration register
+    word_t 
         prid,       // 15, Processor identification and revision
         epc;        // 14, Program counter at last exception, R/W
-    cp0_cause_t
+    cp0_cause_t 
         cause;      // 13, Cause of last general exception
     cp0_status_t
         status;     // 12, Processor status and control
     word_t
-        compare,    // 11, Timer interrupt control, R/W, normally write only
-        entryhi,    // 10, High-order portion of the TLB entry
+        compare;    // 11, Timer interrupt control, R/W, normally write only
+    cp0_entryhi_t
+        entryhi;    // 10, High-order portion of the TLB entry
+    word_t
         count,      // 09, Processor cycle count, R/W
         badvaddr,   // 08, Reports the address for the most recent address-related exception, R
-        hwrena,     // 07, Enables access via the RDHWR instruction to selected hardware registers
-        wired,      // 06, Controls the number of fixed ("wired") TLB entries
-        pagemask,   // 05, Control for variable page size in TLB entries
-        context_,   // 04, Pointer to page table entry in memory
+        hwrena;     // 07, Enables access via the RDHWR instruction to selected hardware registers
+    cp0_wired_t
+        wired;      // 06, Controls the number of fixed ("wired") TLB entries
+    cp0_pagemask_t    
+        pagemask;   // 05, Control for variable page size in TLB entries
+    cp0_context_t
+        context_;   // 04, Pointer to page table entry in memory
+    cp0_entrylo_t
         entrylo1,   // 03, Low-order portion of the TLB entry for odd-numbered virtual pages
-        entrylo0,   // 02, Low-order portion of the TLB entry for even-numbered virtual pages
-        random,     // 01, Randomly generated index into the TLB array
+        entrylo0;   // 02, Low-order portion of the TLB entry for even-numbered virtual pages
+    cp0_random_t    
+        random;     // 01, Randomly generated index into the TLB array
+    cp0_index_t
         index;       // 00, Index into the TLB array
 } cp0_regs_t;
 
